246e770649ea9171090f8853b6712d85362d7aab
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-09-17 19:35:52 +0300

    add lab 7 and change lab 1

--- lab7/src/main/java/oleksii/leheza/java/lab7/Main.java
+++ lab7/src/main/java/oleksii/leheza/java/lab7/Main.java
@@ -1,14 +1,15 @@
-package oleksii.leheza.java.lab1;
+package oleksii.leheza.java.lab7;
 
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
-		StringFormatter stringFormatter = new StringFormatter();
+		StringHandler stringHandler = new StringHandler();
 		Scanner console = new Scanner(System.in);
 		System.out.print("Input text:");
 		String inputText = console.nextLine();
-		String[] result = stringFormatter.getWordsInIncreasingOrder(inputText);
+		String[] result = stringHandler.getWordsInIncreasingOrder(inputText);
 		System.out.println("---reuslt---");
 		for (String word : result) {
 			System.out.println(word);
--- lab7/src/main/java/oleksii/leheza/java/lab7/StringHandler.java
+++ lab7/src/main/java/oleksii/leheza/java/lab7/StringHandler.java
@@ -1,20 +1,17 @@
-package oleksii.leheza.java.lab1;
+package oleksii.leheza.java.lab7;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class StringFormatter {
+public class StringHandler {
 
 	public static final String DELIMITER = "\\s+";
 
 	public String[] getWordsInIncreasingOrder(String inputText) {
-		if (inputText == null) {
-			return null;
-		}
-		String text = inputText.trim();
-		return Arrays.stream(text.split(DELIMITER)).filter(this::isWordInIncreasingOrder).collect(Collectors.toList())
-				.toArray(new String[0]);
+		return (inputText == null) ? null
+				: Arrays.stream(inputText.trim().split(DELIMITER)).filter(this::isWordInIncreasingOrder)
+						.collect(Collectors.toList()).toArray(new String[0]);
 	}
 
 	private boolean isWordInIncreasingOrder(String word) {
